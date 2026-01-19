53af4af0a61540e88cb661d2d5446d860ece886c
gatik1234err <gatikbattapadi@gmail.com>
2026-01-19 14:44:32 +0530

    Aur bhi kuch cheezze

---
+++ Javascript/a.js
@@ -0,0 +1,8 @@
+function greetings() {
+  console.log("console.log(' print ('Hello World')' ");
+  let a = 12;
+  let b = 4;
+  let c = a / b;
+  document.writeln("The result is: " + c);
+}
+greetings();
